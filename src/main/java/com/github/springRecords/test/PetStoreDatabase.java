6814aa3f46eb9cbb465b999a8597803bffd445b1
Humberto Hernandez <humbertook@gmail.com>
2014-03-22 15:12:38 -0400

    added examples

---
+++ src/main/java/com/github/springRecords/test/PetStoreDatabase.java
@@ -0,0 +1,11 @@
+package com.github.springRecords.test;
+
+import javax.sql.DataSource;
+
+public class PetStoreDatabase extends BaseDatabase {
+
+	public PetStoreDatabase(DataSource dataSource) {
+		super(dataSource);
+	}
+
+}
---
+++ src/test/java/com/github/springRecords/SimpleQueries.java
@@ -0,0 +1,61 @@
+package com.github.springRecords;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import com.github.springRecords.test.OwnerRecord;
+import com.github.springRecords.test.PetStoreDatabase;
+import com.mysql.jdbc.Driver;
+
+public class SimpleQueries {
+
+    public DataSource createDs() {
+        SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        ds.setDriverClass(Driver.class);
+        ds.setUrl("jdbc:mysql://localhost:3306/test");
+        ds.setUsername("root");
+        ds.setPassword("mysql");
+        return ds;
+    }
+
+    @Test
+    public void queryAll() {
+
+    	PetStoreDatabase petStoreDb = new PetStoreDatabase(createDs());
+
+    	List<OwnerRecord> all = petStoreDb.getOwnerTable().queryAll();
+
+    	assertTrue(all.size() >= 0);
+
+    }
+
+    @Test
+    public void queryOne() {
+
+    	PetStoreDatabase petStoreDb = new PetStoreDatabase(createDs());
+
+    	OwnerRecord ownerRecord = petStoreDb.getOwnerTable().findOwnerById(10);
+
+    	assertEquals("Humberto", ownerRecord.name);
+
+    }
+
+
+    @Test
+    public void querySome() {
+    	PetStoreDatabase petStoreDb = new PetStoreDatabase(createDs());
+
+    	List<OwnerRecord> result = petStoreDb.getOwnerTable().findByName("Humberto");
+
+    	assertTrue(result.size() >= 0);
+
+    }
+
+}
